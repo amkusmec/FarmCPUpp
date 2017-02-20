a88eefec7b5ab4020ea39e3c1835d7fd5be31cb3
Aaron Kusmec <amkusmec@iastate.edu>
2017-02-19 21:10:11 -0600

    Made manhattan_plot more flexible.

--- R/manhattan_plot.R
+++ R/manhattan_plot.R
@@ -9,13 +9,14 @@
 #' @param cutoff A significance cutoff (default = \code{NULL}).
 #' @param ylab A label for the y-axis (default =
 #'   \code{expression(paste(-log[10], "p-value"))}).
+#' @param ... Other graphics arguments.
 #'
 #' @author Aaron Kusmec
 #'
 #' @export
 
 manhattan_plot <- function(res, colors = c("black", "grey80"), cutoff = NULL,
-                           ylab = expression(paste(-log[10], "p-value"))) {
+                           ylab = expression(paste(-log[10], "p-value")), ...) {
   # Check for appropriate column names
   if (!("Chromosome" %in% names(res))) stop("No column 'Chromosome.'")
   if (!("Position" %in% names(res))) stop("No column 'Position'.")
@@ -39,16 +40,13 @@ manhattan_plot <- function(res, colors = c("black", "grey80"), cutoff = NULL,
   breakpoints <- data.frame(Chromosome = 1:length(breakpoints),
                             Position = breakpoints)
 
-  .pardefault <- par(no.readonly = TRUE)
-  par(mar = c(4.1, 4.1, 1.1, 1.1))
   ylim <- c(0, max(-log10(res$p.value*0.1)))
   plot(1, type = "n", xlim = c(1, max(res$Order)), ylim = ylim,
-       main = "", xlab = "", ylab = ylab, xaxt = "n", las = 2)
+       xlab = "", ylab = ylab, xaxt = "n", las = 2, ...)
   axis(1, at = breakpoints$Position, labels = paste("chr", breakpoints$Chromosome),
        las = 2)
-  points(res$Order, -log10(res$p.value), pch = 19, cex = 0.5, col = colors[res$Color])
+  points(res$Order, -log10(res$p.value), col = colors[res$Color], ...)
   if (!is.null(cutoff)) {
     abline(h = -log10(cutoff), lwd = 2, lty = 2, col = "red")
   }
-  par(.pardefault)
 }
